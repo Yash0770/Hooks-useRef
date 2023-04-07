50bd7f5d6cc797a4df1e11b5b19c20512edc58cb
Yash <100015221+Yash0770@users.noreply.github.com>
2023-04-07 15:09:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookTimer from './Components/HookTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <HookTimer/>
+    </div>
+  );
+}
+
+export default App;
